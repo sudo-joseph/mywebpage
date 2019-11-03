b69ae2045385a6172a633d61801a8aeaaca38c2e
Joseph Reid <joseph.james.reid@gmail.com>
2019-11-02 21:54:18 -0700

    Update build.sh for main pages. Updated content pages to fix broken image links.

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+#build index.html
 cat templates/top.html content/index.html templates/bottom.html > index.html
+
+#build projects.html
+cat templates/top.html content/projects.html templates/contact.html > projects.html
+
+#build contact.html
+cat templates/top.html content/contact.html templates/contact.html > contact.html
