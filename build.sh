5c251795d5f31c411711476f1093eedf582bc985
Joseph Reid <joseph.james.reid@gmail.com>
2019-11-02 23:35:51 -0700

    Updated build.sh to place files in correct directory. Updated top.html and content files to refernce new directory structure. Deleted old site html files in project root directory.

--- build.sh
+++ build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 #build index.html
-cat templates/top.html content/index.html templates/bottom.html > index.html
-sed '/<li class="nav-item home">/ c\          <li class="nav-item home active">' index.html > temp.html
-sed '/<a class="nav-link" href="index.html">Home<\/a>/ c\         <a class="nav-link active" href="index.html">Home</a>' temp.html > index.html
+cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+sed '/<li class="nav-item home">/ c\          <li class="nav-item home active">' docs/index.html > temp.html
+sed '/<a class="nav-link" href="index.html">Home<\/a>/ c\         <a class="nav-link active" href="index.html">Home</a>' temp.html > docs/index.html
 
 #build projects.html
 
-cat templates/top.html content/projects.html templates/bottom.html > projects.html
-sed '/<li class="nav-item projects">/ c\          <li class="nav-item projects active">' projects.html > temp.html
-sed '/<a class="nav-link" href="projects.html">Projects<\/a>/ c\         <a class="nav-link active" href="projects.html">Projects</a>' temp.html > projects.html
+cat templates/top.html content/projects.html templates/bottom.html > docs/projects.html
+sed '/<li class="nav-item projects">/ c\          <li class="nav-item projects active">' docs/projects.html > temp.html
+sed '/<a class="nav-link" href="projects.html">Projects<\/a>/ c\         <a class="nav-link active" href="projects.html">Projects</a>' temp.html > docs/projects.html
 
 #build contact.html
 
-cat templates/top.html content/contact.html templates/bottom.html > contact.html
-sed '/<li class="nav-item contact">/ c\          <li class="nav-item contact active">' contact.html > temp.html
-sed '/<a class="nav-link" href="contact.html">Contact Me<\/a>/ c\         <a class="nav-link active" href="contact.html">Contact Me</a>' temp.html > contact.html
+cat templates/top.html content/contact.html templates/bottom.html > docs/contact.html
+sed '/<li class="nav-item contact">/ c\          <li class="nav-item contact active">' docs/contact.html > temp.html
+sed '/<a class="nav-link" href="contact.html">Contact Me<\/a>/ c\         <a class="nav-link active" href="contact.html">Contact Me</a>' temp.html > docs/contact.html
 
 rm temp.html
