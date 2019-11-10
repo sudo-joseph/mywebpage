8fdbf1c7e8bf883cf7b979730c36a096da490483
Joseph Reid <joseph.james.reid@gmail.com>
2019-11-10 11:51:04 -0800

    Working blogpost generation.

--- build.py
+++ build.py
@@ -1,53 +1,21 @@
 #!/usr/bin/env python3
 
-from functools import partial
+__author__ = 'Joseph Reid'
+
+#Constants
 
 def main():
-    content = getContent()
-    base ='templates/base.html'
-    template = getPage(base)
-    for page in content:
-        formatting = page['formatting']
-        formatting['content'] = open(page['filename']).read()
-        open(page['output'],'w').write(template.format(**formatting))
-
-def getContent():
     """
-    getContent()
-
-    Generates blog content for templates.
+    main() - Main Loop for Static Site Generator.
     """
 
-    return [{'filename':'content/index.html',
-                'output':'docs/index.html',
-                'formatting':{'title':'Joseph\'s Blog',
-                              'index':'active',
-                              'projects':'',
-                              'contact':'',
-                               }},
-               {'filename':'content/projects.html',
-               'output':'docs/projects.html',
-               'formatting':{'title':'Joseph\'s Projects',
-                             'index':'',
-                             'projects':'active',
-                             'contact':'',
-                              }},
-              {'filename':'content/contact.html',
-              'output':'docs/contact.html',
-              'formatting':{'title':'Contact Me',
-                            'index':'',
-                            'projects':'',
-                            'contact':'active',
-                         }}]
-
-def genBlogPosts():
-    index_formatting = {'title':'Joseph\'s Blog',
+    INDEX_FORMTATTING = {'title':'Joseph\'s Blog',
                               'index':'active',
                               'projects':'',
                               'contact':'',
                                }
 
-    blog_posts = [{ 'content_file':'blog/JosephLanes.html',
+    BLOG_POSTS = [{ 'content_file':'blog/JosephLanes.html',
                     'ouput_file':'docs/JosephLanes.html',
                     'formatting':{'blog_title':'A plan to fix I-80 in Berkeley (for me)',
                                   'publication_date':'2019-11-09',
@@ -57,6 +25,37 @@ def genBlogPosts():
                                   'blog_text':'',
                                    }},
                                    ]
+
+    OTHER_PAGES = [{'filename':'content/index.html',
+                'output':'docs/index.html',
+                'formatting':{'title':'Joseph\'s Blog',
+                              'index':'active',
+                              'projects':'',
+                              'contact':'',
+                               }},
+                   {'filename':'content/projects.html',
+                   'output':'docs/projects.html',
+                   'formatting':{'title':'Joseph\'s Projects',
+                                 'index':'',
+                                 'projects':'active',
+                                 'contact':'',
+                                  }},
+                  {'filename':'content/contact.html',
+                  'output':'docs/contact.html',
+                  'formatting':{'title':'Contact Me',
+                                'index':'',
+                                'projects':'',
+                                'contact':'active',
+                             }}]
+
+    genBlogPosts(BLOG_POSTS,INDEX_FORMTATTING)
+    # genindexPage()
+    # genContentPages()
+
+def genBlogPosts(blog_posts,index_formatting):
+    """
+    genBlogPosts() - Generates blog posts.
+    """
     blog_base ='templates/blog_base.html'
     site_base ='templates/base.html'
     blog_base_template = getPage(blog_base)
@@ -68,7 +67,20 @@ def genBlogPosts():
         index_formatting['content']=blog_base_template.format(**formatting)
         open(post['ouput_file'],'w').write(site_base_template.format(**index_formatting))
 
-def genIndexPage():
+
+def genContentPages():
+    """
+    genContntPages() - Generates other pages.
+    """
+    base ='templates/base.html'
+    template = getPage(base)
+    for page in OTHER_PAGES:
+        formatting = page['formatting']
+        formatting['content'] = open(page['filename']).read()
+        open(page['output'],'w').write(template.format(**formatting))
+
+
+def genIndexPage(blog_posts,index_formatting):
     return #to-do fix broken code here .
     index_blog_entry = 'templates/index_blog_entry_base.html'
     index_blog_post_template = getPage(index_blog_entry)
