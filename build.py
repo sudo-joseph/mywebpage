dc2e3abe21e4c12a36ffab0b792704b89ac0e385
Joseph Reid <joseph.james.reid@gmail.com>
2019-11-17 01:15:22 -0800

    Updated projects.html to include new project page. Minor cleanup of build.py
    regenerated site pages.

--- build.py
+++ build.py
@@ -21,7 +21,7 @@ def main():
 
     #generate blog posts based on content in blog/
     gen_content_posts("blog",CONTENT_BASE,SITE_BASE,JINJA_ENV)
-    gen_index_page()
+    # gen_index_page()
 
     #generate project posts based on content in project/
     gen_content_posts("projects",CONTENT_BASE,SITE_BASE,JINJA_ENV)
@@ -98,24 +98,25 @@ def gen_content_posts(page_dir,content_base,site_base,jinja_env):
         open(os.path.join("docs",os.path.splitext(page)[0] + '.html')
                 ,'w').write(output_file)
 
-
-#     blog_posts = gen_blog_post_list()
-#     other_pages = gen_other_pages()
+# def gen_index_page(blog_posts,index_page,index_formatting,site_base,blog_preview_base,index_base):
+#     """
+#     gen_index_page(blog_posts,index_formatting,blog_base,site_base)
 #
-#     INDEX_PAGE = 'docs/index.html'
-#     INDEX_FORMATTING = {'title':'Joseph\'s Blog',
-#                               'index':'active',
-#                               'projects':'',
-#                               'contact':'',
-#                                }
-#     BLOG_BASE ='templates/blog_base.html'
-#     SITE_BASE ='templates/base.html'
-#     BLOG_PREVIEW_BASE='templates/index_blog_preview_base.html'
-#     INDEX_BASE = 'templates/index_base.html'
+#     Generates blog post preview elements on site landing page.
+#     """
 #
-#     gen_blog_posts(blog_posts,INDEX_FORMATTING,BLOG_BASE,SITE_BASE)
-#     gen_index_page(blog_posts,INDEX_PAGE,INDEX_FORMATTING,SITE_BASE,BLOG_PREVIEW_BASE,INDEX_BASE)
-#     gen_content_pages(SITE_BASE,other_pages)
+#     index_blog_preview_template = get_page(blog_preview_base)
+#     blog_post_previews = ''
+#     for post in blog_posts:
+#         formatting = post['formatting']
+#         blog_content = get_page(post['content_file'])
+#         first_par = ''.join(blog_content.split('</p>')[:2]) + '</p>'
+#         formatting['blog_text']=first_par #need to truncate blog text better
+#         blog_post_previews += index_blog_preview_template.format(**formatting)
+#     site_template = get_page(site_base)
+#     index_template = get_page(index_base)
+#     index_formatting['content'] = index_template.format(blog_posts=blog_post_previews)
+#     open(index_page,'w').write(site_template.format(**index_formatting))
 #
 # def gen_blog_post_list():
 #     blog_dict = {'content_file':'',
@@ -139,83 +140,6 @@ def gen_content_posts(page_dir,content_base,site_base,jinja_env):
 #         output.append(copy.deepcopy(blog_dict))
 #     print(output)
 #     return output
-
-
-# def gen_other_pages():
-#     return [{'filename':'content/projects.html',
-#                    'output':'docs/projects.html',
-#                    'formatting':{'title':'Joseph\'s Projects',
-#                                  'index':'',
-#                                  'projects':'active',
-#                                  'contact':'',
-#                                   }},
-#                   {'filename':'content/contact.html',
-#                   'output':'docs/contact.html',
-#                   'formatting':{'title':'Contact Me',
-#                                 'index':'',
-#                                 'projects':'',
-#                                 'contact':'active',
-#                              }}]
-#
-#     INDEX_PAGE = 'docs/index.html'
-#     INDEX_FORMATTING = {'title':'Joseph\'s Blog',
-#                               'index':'active',
-#                               'projects':'',
-#                               'contact':'',
-#                                }
-#
-#     BLOG_BASE ='templates/blog_base.html'
-#     SITE_BASE ='templates/base.html'
-#     BLOG_PREVIEW_BASE='templates/index_blog_preview_base.html'
-#     INDEX_BASE = 'templates/index_base.html'
-#
-#     gen_blog_posts(BLOG_POSTS,INDEX_FORMATTING,BLOG_BASE,SITE_BASE)
-#     gen_index_page(BLOG_POSTS,INDEX_PAGE,INDEX_FORMATTING,SITE_BASE,BLOG_PREVIEW_BASE,INDEX_BASE)
-#     gen_content_pages(SITE_BASE,OTHER_PAGES)
-#     # addCopyRight()
-#
-
-# def gen_index_page(blog_posts,index_page,index_formatting,site_base,blog_preview_base,index_base):
-#     """
-#     gen_index_page(blog_posts,index_formatting,blog_base,site_base)
-#
-#     Generates blog post preview elements on site landing page.
-#     """
-#
-#     index_blog_preview_template = get_page(blog_preview_base)
-#     blog_post_previews = ''
-#     for post in blog_posts:
-#         formatting = post['formatting']
-#         blog_content = get_page(post['content_file'])
-#         first_par = ''.join(blog_content.split('</p>')[:2]) + '</p>'
-#         formatting['blog_text']=first_par #need to truncate blog text better
-#         blog_post_previews += index_blog_preview_template.format(**formatting)
-#     site_template = get_page(site_base)
-#     index_template = get_page(index_base)
-#     index_formatting['content'] = index_template.format(blog_posts=blog_post_previews)
-#     open(index_page,'w').write(site_template.format(**index_formatting))
-#
-# def gen_content_pages(site_base,other_pages):
-#     """
-#     genContntPages() - Generates other pages.
-#     """
-#     template = get_page(site_base)
-#     for page in other_pages:
-#         formatting = page['formatting']
-#         formatting['content'] = open(page['filename']).read()
-#         open(page['output'],'w').write(template.format(**formatting))
-
-
-
-
-# def addCopyRight(index_page,blog_posts,other_pages):
-#     year = datetime.datetime.now().year
-#     blogpages = [page['ouput_file'] for page in blog_posts]
-#
-#     other_pages =
-#
-#     pages = index_page +
-
 
 if __name__ == '__main__':
     main()
