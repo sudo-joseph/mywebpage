d61aca23127831a979e7dacbc8bb1363da564713
Joseph Reid <joseph.james.reid@gmail.com>
2019-11-26 14:35:11 -0800

    Changes for lint; update to projects view to pull github data.

--- views.py
+++ views.py
@@ -1,25 +1,42 @@
-import requests
-from django.http import HttpResponse
+"""
+View functions for my django site.
+
+This script can be used to generate a new blog page based on template, and
+provides some helper functions for generating content for site pages.
+"""
+
+# import requests
+# from django.http import HttpResponse
 from django.shortcuts import render
-import datetime
-import glob
-import os
-import copy
-import markdown
+# import datetime
+# import glob
+# import os
+# import copy
+# import markdown
 import utils
 
+
 def index(request):
-    options=utils.gen_preview_pages("blog","index")
+    """Render index page."""
+    options = utils.gen_preview_pages("blog", "index")
     return render(request, 'preview_base.html', options)
 
+
 def projects(request):
-    context = utils.github_api(request)
+    """Render projects page."""
+    context = utils.github_repos(request)
     return render(request, 'git_repo_base.html', context)
 
+
 def contact(request):
-    options=utils.gen_content_post('contact','content','contact')
+    """Render contact page."""
+    options = utils.gen_content_post('contact', 'content', 'contact')
     return render(request, 'content_base.html', options)
 
-def post(request,title,root):
-    options=utils.gen_content_post(title,request.resolver_match.url_name,root)
+
+def post(request, title, root):
+    """Render blog post pages."""
+    options = utils.gen_content_post(title,
+                                     request.resolver_match.url_name,
+                                     root)
     return render(request, 'content_base.html', options)
